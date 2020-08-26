9c4cf639849ebec5fa69ee2daf8e15ba8905ccb6
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-26 16:15:24 +0800

    [mocking][Alisa & Yancy] init

---
+++ src/test/java/parking/InOrderParkingStrategyTest.java
@@ -0,0 +1,75 @@
+package parking;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class InOrderParkingStrategyTest {
+
+	@Test
+    public void testCreateReceipt_givenACarAndAParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
+
+	    /* Exercise 1, Write a test case on InOrderParkingStrategy.createReceipt()
+	    * With using Mockito to mock the input parameter */
+
+    }
+
+    @Test
+    public void testCreateNoSpaceReceipt_givenACar_thenGiveANoSpaceReceipt() {
+
+        /* Exercise 1, Write a test case on InOrderParkingStrategy.createNoSpaceReceipt()
+         * With using Mockito to mock the input parameter */
+
+    }
+
+    @Test
+    public void testPark_givenNoAvailableParkingLot_thenCreateNoSpaceReceipt(){
+
+	    /* Exercise 2: Test park() method. Use Mockito.spy and Mockito.verify to test the situation for no available parking lot */
+
+    }
+
+    @Test
+    public void testPark_givenThereIsOneParkingLotWithSpace_thenCreateReceipt(){
+
+        /* Exercise 2: Test park() method. Use Mockito.spy and Mockito.verify to test the situation for one available parking lot */
+
+    }
+
+    @Test
+    public void testPark_givenThereIsOneFullParkingLot_thenCreateReceipt(){
+
+        /* Exercise 2: Test park() method. Use Mockito.spy and Mockito.verify to test the situation for one available parking lot but it is full */
+
+    }
+
+    @Test
+    public void testPark_givenThereIsMultipleParkingLotAndFirstOneIsFull_thenCreateReceiptWithUnfullParkingLot(){
+
+        /* Exercise 3: Test park() method. Use Mockito.spy and Mockito.verify to test the situation for multiple parking lot situation */
+
+    }
+
+
+    private Car createMockCar (String carName) {
+	    Car car = mock(Car.class);
+	    when(car.getName()).thenReturn(carName);
+	    return car;
+    }
+
+    private ParkingLot createMockParkingLot (String parkingLotName, int maxCapacity) {
+	    ParkingLot parkingLot = mock(ParkingLot.class);
+	    when(parkingLot.getName()).thenReturn(parkingLotName);
+	    when(parkingLot.getMaxCapacity()).thenReturn(maxCapacity);
+	    return parkingLot;
+    }
+
+}
